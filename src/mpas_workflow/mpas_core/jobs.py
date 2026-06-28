a14f328b5e70d7c3e2a2809c269e1242b3ba7cce
João Gerd Zell de Mattos <joao.gerd@gmail.com>
2026-06-27 23:15:05 -0300

    Add wait option to MPAS forecast submit

--- src/mpas_workflow/mpas_core/jobs.py
+++ src/mpas_workflow/mpas_core/jobs.py
@@ -4,8 +4,8 @@ from ..shell import qsub, require_file
 from .model import forecast_run_dir
 
 
-def run_job(config, init_time: str, lead_hours: int, dt=None):
+def run_job(config, init_time: str, lead_hours: int, dt=None, wait: bool = False):
     dt = int(dt or config["runtime"]["config_dt"])
     run_dir = forecast_run_dir(config, init_time, lead_hours, dt)
     require_file(run_dir / "run_mpas_forecast.pbs", "PBS de forecast")
-    return qsub("run_mpas_forecast.pbs", run_dir)
+    return qsub("run_mpas_forecast.pbs", run_dir, block=wait)
