ea1c50ac4f932469c6723ffb79d2b60575f9ab2b
João Gerd Zell de Mattos <joao.gerd@gmail.com>
2026-06-30 12:13:38 -0300

    Use improved presentation renderer

--- src/mpas_workflow/bmatrix_presentation.py
+++ src/mpas_workflow/bmatrix_presentation.py
@@ -1,5 +1,5 @@
 """Presentation figure entry point."""
-from mpas_workflow.bmatrix_presentation_impl import main
+from mpas_workflow.bmatrix_presentation_renderer import main
 
 if __name__ == "__main__":
     raise SystemExit(main())
