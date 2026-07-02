13376b01a9e51fe3e702c42efa99e5efcbc7e7a9
João Gerd Zell de Mattos <joao.gerd@gmail.com>
2026-07-01 22:32:00 -0300

    fix(nmc): remove incomplete package initializer

--- src/mpas_workflow/nmc_core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""NMC campaign manifest validation for the B-matrix consumer workflow."""
-
-from .checks import validate_manifest
-from .manifest import read_manifest
-
-__all__ = ["read_manifest", "validate_manifest"]
